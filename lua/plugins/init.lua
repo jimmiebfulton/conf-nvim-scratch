b9b8a187922f0613c6a3c780836da87aabd99c6f
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-01-29 23:01:42 -0800

    init: Expand Snacks config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -30,4 +30,21 @@ return {
       end
     end,
   },
+
+  -- UI components
+  { "MunifTanjim/nui.nvim", lazy = true },
+
+  {
+    "snacks.nvim",
+    opts = {
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = false }, -- we set this in options.lua
+      toggle = { map = LazyVim.safe_keymap_set },
+      words = { enabled = true },
+    },
+  },
 }
