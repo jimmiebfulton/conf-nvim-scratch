bc00bfcc405d65903c5ed7bb2e002369878a1d75
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-01-30 22:29:37 -0800

    snacks: Terminal and Scratch

---
+++ lua/plugins/scratch.lua
@@ -0,0 +1,12 @@
+return {
+  -- Snacks utils
+  {
+    "snacks.nvim",
+    -- stylua: ignore
+    keys = {
+      { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+      { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+      { "<leader>dps", function() Snacks.profiler.scratch() end, desc = "Profiler Scratch Buffer" },
+    },
+  },
+}
