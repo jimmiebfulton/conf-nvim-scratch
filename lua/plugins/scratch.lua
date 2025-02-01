72cb810c0fc995af9c09b3f94c643a9211c0968c
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-01-31 20:29:30 -0800

    scratch: Remove profiler scratch smallfix

--- lua/plugins/scratch.lua
+++ lua/plugins/scratch.lua
@@ -6,7 +6,6 @@ return {
     keys = {
       { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
       { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
-      { "<leader>dps", function() Snacks.profiler.scratch() end, desc = "Profiler Scratch Buffer" },
     },
   },
 }
