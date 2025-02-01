a2bffe2227bdbff844d5f1902909bc03911af302
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-01-31 22:27:36 -0800

    yazi: Update config

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -24,8 +24,10 @@ return {
     opts = {
       -- if you want to open yazi instead of netrw, see below for more info
       open_for_directories = true,
+      open_multiple_tabs = true,
       keymaps = {
         show_help = "<f1>",
+        cycle_open_buffers = "<tab>",
       },
     }
 
