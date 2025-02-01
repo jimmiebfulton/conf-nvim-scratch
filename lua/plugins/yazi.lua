9f9589b0f977fb32a70a3a3e730f2afde4cce879
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-01-31 20:29:30 -0800

    yazi: remove unnecessary bindings

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -10,26 +10,20 @@ return {
         "<leader>fy",
         mode = { "n", "v" },
         "<cmd>Yazi<cr>",
-        desc = "Open yazi at the current file",
-      },
-      {
-        -- Open in the current working directory
-        "<leader>cw",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Open the file manager in nvim's working directory",
+        desc = "Open Yazi (cwd)",
       },
       {
         -- NOTE: this requires a version of yazi that includes
         -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-        "<c-up>",
+        "<leader>fY",
         "<cmd>Yazi toggle<cr>",
-        desc = "Resume the last yazi session",
+        desc = "Resume Yazi",
       },
     },
     ---@type YaziConfig
     opts = {
       -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = false,
+      open_for_directories = true,
       keymaps = {
         show_help = "<f1>",
       },
