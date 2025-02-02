0bdbeeee29897dd722909394e6b82fa86f2d5cae
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-02-02 15:37:47 -0800

    search: Add picker for Undo

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -6,6 +6,7 @@ return {
     -- stylua: ignore
     keys = {
       { "<leader>sp",  function() Snacks.picker.pick() end, desc = "Pickers" },
+      { "<leader>su",  function() Snacks.picker.undo() end, desc = "Undo" },
     },
   },
 }
