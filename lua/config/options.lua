f2d547ca0b67e5719e0379b6728d45ce5f1f8c55
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-02-02 21:10:01 -0800

    neovide: add options for neovide
    grapple: Change WhichKey titles

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,12 @@
 -- Add any additional options here
 vim.g.root_spec = { "lsp", { ".jj", ".git", "lua" }, "cwd" }
 vim.opt.timeoutlen = 500
+
+if vim.g.neovide then
+    vim.g.neovide_cursor_vfx_mode = "torpedo"
+    vim.g.neovide_cursor_vfx_particle_lifetime = 2.0
+    vim.g.neovide_cursor_vfx_opacity = 400.0
+    vim.g.neovide_fullscreen = true
+    vim.g.neovide_transparency = 0.95
+    vim.g.neovide_normal_opacity = 0.95
+end
