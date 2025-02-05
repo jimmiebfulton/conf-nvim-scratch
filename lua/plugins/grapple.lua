27064aba236274a6caec76b007833f0152c20291
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-02-04 19:55:39 -0800

    grapple: Swap mm and nM
    reformat
    formatting

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    "cbochs/grapple.nvim",
-    opts = {
-        scope = "git", -- also try out "git_branch"
-    },
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = "Grapple",
-    keys = {
-        { "<leader>m", desc = "Grapple" },
-        { "<leader>mm", "<cmd>Grapple toggle<cr>", desc = "Grapple Toggle Tag" },
-        { "<leader>mM", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple Open Tags" },
-        { "<leader>mn", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple Next Tag" },
-        { "<leader>mp", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple Prev Tag" },
-    },
-},
+	{
+		"cbochs/grapple.nvim",
+		opts = {
+			scope = "git", -- also try out "git_branch"
+		},
+		event = { "BufReadPost", "BufNewFile" },
+		cmd = "Grapple",
+		keys = {
+			{ "<leader>m", desc = "Grapple" },
+			{ "<leader>mM", "<cmd>Grapple toggle<cr>", desc = "Grapple Toggle Tag" },
+			{ "<leader>mm", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple Open Tags" },
+			{ "<leader>mn", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple Next Tag" },
+			{ "<leader>mp", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple Prev Tag" },
+		},
+	},
 }
